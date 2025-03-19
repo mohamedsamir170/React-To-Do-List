2abb5687ff2baecb646e2388f14d83ca4f36c074
Mohamed Samir <mohamedsamir170569@gmail.com>
2025-03-19 21:06:07 +0200

    package.json modified

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { TodoProvider } from './context/TodoContext';
 function App() {
   return (
     <TodoProvider>
-      <Router basename="/React-To-Do-List"></Router>
+      {/* <Router basename="/react-to-do-list"></Router> */}
       <Router>
         <div className="App">
           <nav className="navbar">
